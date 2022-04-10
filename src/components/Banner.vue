4fe88b5361c3429d53f7f11025f79809c45db6dc
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-04-10 03:27:40 -0400

    correção na responsividade da home

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -34,48 +34,25 @@ export default {
 
 @media (max-width: 320px) {
   #main-banner {
-    background-image: url('/img/burger.jpg');
-    background-position: -35em center!important;
-    background-attachment: fixed;
-    background-size: cover;
-    background-repeat: no-repeat;
-    height: 800px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
+    background-position-x: -35em!important;
+    background-position-y: center
   }
 
   #main-banner h1 {
-    background: #252525;
-    color: whitesmoke;
     font-size: 1.25em;
     padding: 15px;
-    border-bottom: 3px solid #fcac19;
-    border-right: 3px solid #fcac19;
   }
 }
 
 @media (max-width: 768px) {
   #main-banner {
-    background-image: url('/img/burger.jpg');
     background-position: center center;
-    background-attachment: fixed;
-    background-size: cover;
-    background-repeat: no-repeat;
-    height: 800px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
   }
 
   #main-banner h1 {
-    background: #252525;
-    color: whitesmoke;
     font-size: 1.25em;
-    padding: .9em 2.5em;
-    border-bottom: 3px solid #fcac19;
-    border-right: 3px solid #fcac19;
-    margin-top: 30em;
+    padding: .9em 2.5em;;
+    margin-top: 28em;
     margin-left: 0.2em;
   }
 }
